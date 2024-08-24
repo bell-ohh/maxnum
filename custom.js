d7d256012ed85458de9bd51fa058cd0170417be7
bell-ohh <belloaise@anniecannons.com>
2024-08-23 21:21:17 -0400

    initial commit, check consolelog

---
+++ custom.js
@@ -0,0 +1,10 @@
+let numbers = [3, 100, 2356, 72, 81, 9]
+let max = numbers[0];
+
+for (let i = 1; i < numbers.length; i++) {
+    if (numbers[i] > max) {
+        max = numbers[i];
+    }
+}
+
+console.log(max);
